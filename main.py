e7e3641949f20efebc2b8ba2a330046a15fdb216
Mark Melnic <markhappy200@gmail.com>
2020-10-11 13:38:50 +0200

    Added alternative login method

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 
-import os
+import os, getpass
 import configparser as cfg
 
 from sel_module import *
 
 if __name__ == "__main__":
     parser = cfg.ConfigParser()
-    with open("creds.txt", "r") as creds_file:
-        creds = creds_file.readlines()
+    print("*** Provide your login details.")
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
     dv = boot()
-    login(dv, creds[0].strip("\n"), creds[1])
+    login(dv, username, password)
     for listing in os.listdir():
         if not "." in listing and not "__" in listing:
             parser.read(listing + "/data.cfg")
