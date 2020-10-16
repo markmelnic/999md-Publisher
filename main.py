97a07e43abf13050ed6427828389e8fec445b2f5
Mark Melnic <markhappy200@gmail.com>
2020-10-16 09:12:06 +0200

    Black formatting

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-
 import os, time
 import configparser as cfg
 from argparse import ArgumentParser, RawTextHelpFormatter
 
 from sel_module import *
 
-DAY = 86400 # day to seconds
+DAY = 86400  # day to seconds
 
 if __name__ == "__main__":
     cfgparser = cfg.ConfigParser()
     argparser = ArgumentParser(formatter_class=RawTextHelpFormatter)
-    argparser.add_argument("username", metavar="username", type=str, help="Login username.")
-    argparser.add_argument("password", metavar="password", type=str, help="Login password.")
+    argparser.add_argument(
+        "username", metavar="username", type=str, help="Login username."
+    )
+    argparser.add_argument(
+        "password", metavar="password", type=str, help="Login password."
+    )
     args = argparser.parse_args()
 
     dv = boot()
